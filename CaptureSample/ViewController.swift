94dc2f9d7f6177cbdd6601fc1b8f69e27aadf59a
hachibe <tsnorionori@gmail.com>
2017-10-15 23:21:00 +0900

    pixelBufferの生成と、その生成確認用にpixelBufferからimageを取得する処理を追加

--- CaptureSample/ViewController.swift
+++ CaptureSample/ViewController.swift
@@ -78,6 +78,58 @@ class ViewController: UIViewController {
         capturedImageView.image = image
         UIGraphicsEndImageContext();
 
+        // pixelBufferの生成
+        let width = Int(image.size.width * ratio) / 16 * 16 // 16の倍数に
+        let height = Int(image.size.height * ratio) / 16 * 16
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var buffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         width,
+                                         height,
+                                         kCVPixelFormatType_32ARGB,
+                                         attrs,
+                                         &buffer)
+        guard (status == kCVReturnSuccess) else {
+            fatalError("CVPixelBufferCreate status not success")
+        }
+        guard let pixelBuffer = buffer else {
+            fatalError("CVPixelBufferCreate failed to create pixelBuffer")
+        }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelData,
+                                      width: width,
+                                      height: height,
+                                      bitsPerComponent: 8,
+                                      bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
+                                      space: rgbColorSpace,
+                                      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+                                        fatalError("CGContext from pixelBuffer failed")
+        }
+        context.translateBy(x: 0, y: image.size.height * ratio)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context)
+        image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        // pixelBufferからimageを取得(imageからpixelBufferが生成されているかの確認用)
+//        let ciImage = CIImage(cvImageBuffer: pixelBuffer)
+//        let temporaryContext = CIContext(options: nil)
+//        let imageRect = CGRect(x: 0,
+//                               y: 0,
+//                               width: CGFloat(CVPixelBufferGetWidth(pixelBuffer)),
+//                               height: CGFloat(CVPixelBufferGetHeight(pixelBuffer)))
+//        guard let videoImage = temporaryContext.createCGImage(ciImage, from: imageRect) else {
+//            fatalError("createCGImage failed")
+//        }
+//        let uiImage = UIImage(cgImage: videoImage)
+        
         let time = Date().timeIntervalSince(startDate)
         print("captureWindowTime: \(time)")
     }
